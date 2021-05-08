635e4595b7d630b791e69f783eec806a93fc3c13
Swampy691 <63923385+ivymmorrison@users.noreply.github.com>
2021-05-08 16:06:10 +1200

    Exercise_04 completed

--- src/labs_examples/fundamentals/labs/Exercise_04.java
+++ src/labs_examples/fundamentals/labs/Exercise_04.java
@@ -16,6 +16,24 @@ public class Exercise_04 {
         System.out.println("int i is: " + i);
 
         // write your code below
+        byte myNum = 100;
+        System.out.println("byte myNum is: " + myNum);
+        long myLNum = 15000000000L;
+        System.out.println("long myLNum is: " +myLNum);
+        float myFloatNum = 5.99f;    // Floating point number
+        System.out.println("float myFloatNum is: " + myFloatNum);
+        char myLetter = 'D';         // Character
+        System.out.println("char myLetter is: " + myLetter);
+        boolean myBool = true;       // Boolean
+        System.out.println("boolean myBool is: " + myBool);
+        String myText = "Hello";     // String
+        System.out.println("String myText is: " + myText);
+        double myDNum = 19.99d;
+        System.out.println("double myDNum is: " +myDNum);
+        float f1 = 35e3f;
+        double d1 = 12E4d;
+        System.out.println("Scientific float is: " + f1);
+        System.out.println("Scientific double is: " + d1);
 
     }
 
