96684c9b9bf7d0b9c6bd5fe3da9b174f4c0fd512
Swampy691 <63923385+ivymmorrison@users.noreply.github.com>
2021-05-16 07:22:22 +1200

    Variable examples and practice

---
+++ src/labs_examples/fundamentals/examples/variable_scope/Vehicle.java
@@ -0,0 +1,15 @@
+package labs_examples.fundamentals.examples.variable_scope;
+
+/**
+ * *Created by imorrison student at coding nomads
+ */
+//POJO = Plain Old Java Object
+public class Vehicle {
+    public String make;
+    public String model;
+    public int year;
+
+    public void someMethod(){
+        String color;
+    }
+}
