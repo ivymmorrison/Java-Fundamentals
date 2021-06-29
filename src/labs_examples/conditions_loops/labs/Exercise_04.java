07887900d602adffa76ce9ba841b683e894edba1
Swampy691 <63923385+ivymmorrison@users.noreply.github.com>
2021-06-30 08:46:13 +1200

    excercise 5, but not working!

--- src/labs_examples/conditions_loops/labs/Exercise_04.java
+++ src/labs_examples/conditions_loops/labs/Exercise_04.java
@@ -4,9 +4,15 @@ package labs_examples.conditions_loops.labs;
  * Conditions and Loops Exercise 4: Even numbers
  *
  *      Using a "for-loop", print out all even numbers from 1-100.
- *
+ * Ivy Morrison Coding Nomad Student
  */
 
 public class Exercise_04 {
+    public static void main(String[] args){
+        // "count = count + 2" can also be written as "count++"
+        for(int count = 2; count < 101; count = count + 2){
+            System.out.println("This is count: " + count);
+        }
+    }
 
 }
--- src/labs_examples/conditions_loops/labs/Exercise_05.java
+++ src/labs_examples/conditions_loops/labs/Exercise_05.java
@@ -1,5 +1,7 @@
 package labs_examples.conditions_loops.labs;
 
+import java.util.Scanner;
+
 /**
  * Conditions and Loops Exercise 5: Calculator
  *
@@ -8,7 +10,10 @@ package labs_examples.conditions_loops.labs;
  *      Print the results to the console.
  *
  *      For example, if a user enters 1 and 100, the output should be:
- *
+ *      Upper sum bounded by f(x) = x^2  x = 0 and x = 2
+ *      base and height
+ *      base = ^x = 2-0/n = 2/n
+ *      height = f(rep)=f(2/h*i)
  *      The sum is: 5050
  *      The average is: 50.5
  *
@@ -16,4 +21,30 @@ package labs_examples.conditions_loops.labs;
  */
 
 public class Exercise_05 {
+    public static void main(String[] args) {
+        // 1) create scanner (don't forget to import Scanner!)
+        Scanner scanner = new Scanner(System.in);
+        // 2) prompt user for high
+        System.out.print("Enter a high number :  ");
+        // 3) assign input to variable as int
+        int high = scanner.nextInt();
+        // 2) prompt user for low
+        System.out.print("Enter a low number :  ");
+        // 3) assign input to variable as int
+        int low = scanner.nextInt();
+        int sum = 0;
+        int count = 0;
+        int i = low;
+        double average = Double.NaN;
+        for(sum+= i;i <= high; count ++) {
+            // 4. Within the for loop add the value of high and low to get the sum
+            // 5. Print out the value of j
+            System.out.println("The sum is: " + sum);
+            average = sum / count;
+            System.out.println("The Average is: " + average);
+        }
+
+
+
+    }
 }
