a4f45e8f8d25b0e91e38bee3985f141601385df1
Swampy691 <63923385+ivymmorrison@users.noreply.github.com>
2022-02-22 17:51:32 +1300

    Array excercise 02

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -5,17 +5,55 @@ package labs_examples.arrays.labs;
  *
  *      Using the array below, take in a number from 1-10 from the user and print out the index of that
  *      element.
- *
+ *  Ivy Morrison Coding Nomads student 17/02/2022
  */
-
+import java.util.Scanner;
 public class Exercise_02 {
 
-    public static void main(String[] args) {
 
-        int[] array = {10, 9, 8, 7, 6, 5, 4, 3, 2, 1};
+        // Linear-search function to find the index of an element
+        public static int findIndex(int arr[], int t)
+        {
 
-        // write code here
+            // if array is Null
+            if (arr == null) {
+                return -1;
+            }
 
+            // find length of array
+            int len = arr.length;
+            int i = 0;
 
+            // traverse in the array
+            while (i < len) {
+
+                // if the i-th element is t
+                // then return the index
+                if (arr[i] == t) {
+                    return i;
+                }
+                else {
+                    i = i + 1;
+                }
+            }
+            return -1;
+        }
+
+        // Driver Code
+        public static void main(String[] args)
+        {
+            int[] my_array = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+            int num;
+            int idx;
+            for(int i = 0; i <1; i++) {
+                System.out.println("Enter a number from 1-10: ");
+                Scanner keyboard = new Scanner(System.in);
+                num = keyboard.nextInt();
+                idx = my_array.indexOf(num);
+            }
+
+            // find the index of number entered
+            System.out.println("Index of element " +num +" is: " + idx);
+
+        }
     }
-}
