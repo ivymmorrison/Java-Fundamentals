31637f8d3a3cc3956b8f867415f85d2a50b6bbd0
Swampy691 <63923385+ivymmorrison@users.noreply.github.com>
2022-02-27 18:55:46 +1300

    excercise 3 Array enter the commit message for your changes. Lines starting

--- src/labs_examples/arrays/labs/Exercise_02.java
+++ src/labs_examples/arrays/labs/Exercise_02.java
@@ -7,26 +7,77 @@ package labs_examples.arrays.labs;
  *      element.
  *  Ivy Morrison Coding Nomads student 17/02/2022
  */
-import java.util.Scanner;
 import java.util.Arrays;
+import java.util.Scanner;
 public class Exercise_02 {
 
 
-    public static void main(String[] args) {
-        int[] IndexNum = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
-        int index=0;
-
-        Scanner input = new Scanner(System.in);
-
-        System.out.println("Enter number between 1-10:");
-
-        for(int i=0; i< IndexNum.length;i++){
-            if (IndexNum[i]==input.nextInt()) {
+    public static boolean verify;
+    public static int findElementIndex (int value, int [] a)
+    {
+        //the index we return
+        int index = 0;
+        //check whether the value exists in the array
+        for (int i = 0; i< a.length; i++)
+        {
+            if (a[i] == value)
+            {
+                //if value exists, assign i to the variable index
                 index = i;
-                }
-
+                //set verify to true
+                verify = true;
             }
-        System.out.println("The index is: " +index);
-            
+        }
+        return index;
+    }
+    public static void main(String[] args) {
+        //get the array's length as an input from user
+        //Scanner s = new Scanner (System.in);
+        //System.out.println("enter the array's length");
+        //int array_length = s.nextInt();
+
+        //create the array
+        int [] array = new int [10];
+
+        // populate array
+        array[0] = 1;
+        array[1] = 2;
+        array[2] = 3;
+        array[3] = 4;
+        array[4] = 5;
+        array[5] = 6;
+        array[6] = 7;
+        array[7] = 8;
+        array[8] = 9;
+        array[9] = 10;
+
+        //set the array elements
+
+        //System.out.println("Set the array element");
+        //for (int i = 0; i<array_length; i++)
+        //{
+        //    array[i] = s.nextInt();
+        //}
+        System.out.println("The array elements are: " + Arrays.toString(array));
+
+        //set the value you would like to check the index of
+        //get the array's length as an input from user
+        Scanner s = new Scanner (System.in);
+        System.out.println("What element do you want to check the index of?");
+        int element = s.nextInt();
+
+        //send element and array to findElementIndex
+        int index = findElementIndex(element, array);
+
+        //check whether or not the sent value corresponds to any of the
+        //array elements
+        if (verify)
+        {
+            System.out.println("the index of " +element + " is " + index);
+        }
+        else
+        {
+            System.out.println("Sorry, this value doesn't exist!");
+        }
     }
 }
--- src/labs_examples/arrays/labs/Exercise_03.java
+++ src/labs_examples/arrays/labs/Exercise_03.java
@@ -3,7 +3,7 @@ package labs_examples.arrays.labs;
 /**
  *  2D Array
  *
- *      Creat and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
+ *      Create and populate a 5x5 2D array with multiples of 3, starting with 3. Once populated, print out the results.
  *
  *      The output should look something like this:
  *
@@ -12,12 +12,24 @@ package labs_examples.arrays.labs;
  *      ...
  *      ...
  *      ...
- *
+ *   Ivy Morrison 24/02/2022
  */
 
 public class Exercise_03 {
-
     public static void main(String[] args) {
-        
+
+        // please demonstrate how to declare a Two-Dimensional Array
+        // with a height of 5 and a width of 5 below
+        int [] [] twoD = new int [5] [5];
+        // using a nested for loop to populate every index in the 2-D Array
+        for (int i = 1; i < twoD.length; i++){
+            for (int j = 1; j < twoD[i].length; j++){
+                twoD[i] [j] = (i*3)*j;
+                System.out.print(twoD[i][j] + "  ");
+            }
+            // once populated, use a nested for loop to print each element in the 2-D array
+            System.out.println();
+        }
+
     }
 }
